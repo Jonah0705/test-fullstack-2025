62c12d6f72e4c4db3065797af6914c81afe9b2b1
Jonah0705 <jonahpupella@gmail.com>
2025-07-14 09:51:39 +0700

    [Add]

--- question1.py
+++ question1.py
@@ -7,7 +7,7 @@ def f(n: int) -> int:
     power_of_n = 2 ** n
     result = math.ceil(factorial / power_of_n)
     return result
-# insert value in here
+# insert example value in here for it to work
 value = 22
 for i in range(value):
     print(f"f({i}) = {f(i)}")
